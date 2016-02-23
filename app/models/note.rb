fa03a4679cbfc2aa9d894a44394f822b8693aba4
Lance Vincent <lancevincent@gmail.com>
2016-02-23 16:58:27 +0800

    Added paranoia to secure data on delete*

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,5 +2,7 @@ class Note < ActiveRecord::Base
     belongs_to :user
     has_many :assets
 
+    acts_as_paranoid
+
     validates :title, :content, presence: :true, length: {minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222092755) do
+ActiveRecord::Schema.define(version: 20160223081047) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "file"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160222092755) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.datetime "deleted_at"
   end
 
   create_table "users", force: :cascade do |t|
